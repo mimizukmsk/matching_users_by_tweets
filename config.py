95331bf44d7dbc62e2c6e0177cc36a9d4747d9a0
mimizukmsk <enone999@gmail.com>
2018-09-30 23:44:44 +0900

    Change how to manage keys from config.py to .env

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
+from os import environ
+
 # Twitter API
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_SECRET = ""
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_TOKEN = environ['ACCESS_TOKEN']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 # Watson API
-USER_NAME = ""
-PASSWORD = ""
+USER_NAME = environ['USER_NAME']
+PASSWORD = environ['PASSWORD']
 
 def get_consumer_key():
     return CONSUMER_KEY
